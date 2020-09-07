1b16c594f5426cf645b2ca16b37b4f882031df84
nadjaharold <rus0clov@gmail.com>
2020-09-07 22:19:17 +0900

    initial commit

---
+++ practice/q1.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b)
+{
+  int ans = 0;
+  ans = a + b;
+  return ans;
+}
+
+int main()
+{
+  int num1, num2;
+  int sum;
+  cin >> num1 >> num2;
+  sum = solveMeFirst(num1, num2);
+  cout << sum;
+  return 0;
+}
