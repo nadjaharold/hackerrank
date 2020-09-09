0166e7c5892b11edb28b197114819622415f41be
nadjaharold <rus0clov@gmail.com>
2020-09-09 22:43:12 +0900

    solved greedy

---
+++ practice/q6.cpp
@@ -0,0 +1,98 @@
+// Greedy Florist
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the getMinimumCost function below.
+int getMinimumCost(int k, vector<int> c)
+{
+  int len = c.size(), ans = 0, now = 0, count = 0, current = 1, check = 0;
+  sort(c.begin(), c.end(), greater<int>());
+  while (check < len)
+  {
+    for (int i = now; i < len; ++i)
+    {
+      ans += c[i] * current;
+      count++;
+      check++;
+      if (count >= k || check >= len)
+      {
+        count = 0;
+        break;
+      }
+    }
+    now += k;
+    current++;
+  }
+  return ans;
+}
+
+int main()
+{
+  ofstream fout(getenv("OUTPUT_PATH"));
+
+  string nk_temp;
+  getline(cin, nk_temp);
+
+  vector<string> nk = split_string(nk_temp);
+
+  int n = stoi(nk[0]);
+
+  int k = stoi(nk[1]);
+
+  string c_temp_temp;
+  getline(cin, c_temp_temp);
+
+  vector<string> c_temp = split_string(c_temp_temp);
+
+  vector<int> c(n);
+
+  for (int i = 0; i < n; i++)
+  {
+    int c_item = stoi(c_temp[i]);
+
+    c[i] = c_item;
+  }
+
+  int minimumCost = getMinimumCost(k, c);
+
+  fout << minimumCost << "\n";
+
+  fout.close();
+
+  return 0;
+}
+
+vector<string> split_string(string input_string)
+{
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
+
+  input_string.erase(new_end, input_string.end());
+
+  while (input_string[input_string.length() - 1] == ' ')
+  {
+    input_string.pop_back();
+  }
+
+  vector<string> splits;
+  char delimiter = ' ';
+
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+
+  while (pos != string::npos)
+  {
+    splits.push_back(input_string.substr(i, pos - i));
+
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+
+  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+  return splits;
+}
